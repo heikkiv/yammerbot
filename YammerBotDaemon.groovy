8cb3ef4722294d6011d6dcf6ddf40e397f919991
Heikki Verta <heikki.verta@gmail.com>
2012-02-09 21:04:47 +0200

    Fixed message chronology

--- YammerBotDaemon.groovy
+++ YammerBotDaemon.groovy
@@ -4,14 +4,15 @@ YammerBot bot = new YammerBot()
 bot.setVerbose(true)
 bot.setEncoding("utf-8")
 print 'Connecting to irc1.inet.fi ...'
-bot.connect("irc1.inet.fi")
+//bot.connect("irc1.inet.fi")
 println ' connected'
-bot.joinChannel("#ep-dev")
+//bot.joinChannel("#ep-dev")
 println 'Joining #ep-dev'
 
 def token = new File('token.txt').text
 def users = [:]
-int id = getLatestMessageId(token);
+//int id = getLatestMessageId(token);
+int id = 148068991;
 while(true) {
     try {
         def responseAsText = new URL("https://www.yammer.com/api/v1/messages.json?newer_than=${id}&access_token=${token}").text
@@ -19,7 +20,7 @@ while(true) {
         if(response.messages.size() > 0) {
             bot.log("${response.messages.size()} new yammer messages")
             id = response.messages[0].id
-            response.messages.each { message ->
+            response.messages.reverse().each { message ->
                 def name = getUser(message.sender_id, users).full_name
                 bot.relayMessage(name, message.body.plain)
             }
